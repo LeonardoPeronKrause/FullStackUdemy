fae3b647b04e46ea73495e8c6c0afcd3ae282fff
LeonardoPeronKrause <lperonkrause@gmail.com>
2025-01-17 16:38:50 -0300

    Fazendo aula de JS básico e com exercícios

--- projeto/script.js
+++ projeto/script.js
@@ -1,41 +1,31 @@
-var area = document.getElementById('area')
+var peso;
+var altura;
+var resultado;
+var imc;
 
-function entrar() {
-    var nome = prompt('Digite seu nome: ');
+function calc(event) {
+    event.preventDefault();
 
-    if (nome === '' || nome === null) {
-        alert('Ops, algo deu errado!');
-        area.innerHTML = 'Clique no botão para acessar...'
-    } else {
-        area.innerHTML = 'Bem Vindo ' + nome + ' ';
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
 
-        let botaoSair = document.createElement('button');
-        botaoSair.innerText = 'Sair da conta';
-        botaoSair.onclick = sair;
+    imc = peso / (altura * altura);
 
-        area.appendChild(botaoSair);
+    resultado = document.getElementById('resultado');
+
+    if (imc < 17) {
+        resultado.innerHTML = '<br> Seu resultado foi de: ' + imc.toFixed(2) + '<br> Cuidado seu imc está muito abaixo do peso!';
+    } else if (imc > 17 && imc <= 18.49 ) {
+        resultado.innerHTML = '<br> Seu resultado foi de: ' + imc.toFixed(2) + '<br> Cuidado seu imc está abaixo do peso!';
+    } else if (imc > 18.49 && imc <= 24.99) {
+        resultado.innerHTML = '<br> Seu resultado foi de: ' + imc.toFixed(2) + '<br> Parabéns você está no seu peso ideal!';
+    } else if (imc > 24.99 && imc <= 29.99) {
+        resultado.innerHTML = '<br> Seu resultado foi de: ' + imc.toFixed(2) + '<br> Cuidado seu imc está acima do peso!';
+    } else if (imc >= 30) {
+        resultado.innerHTML = '<br> Seu resultado foi de: ' + imc.toFixed(2) + '<br> Cuidado você está em um nível de obesidade';
     }
+
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
+    
 }
-
-function sair() {
-    alert('Até mais!');
-    area.innerHTML = 'Você saiu!';
-}
-
-function mediaAluno(nota1, nota2) {
-    var media = (nota1 + nota2) / 2;
-
-    if (media >= 7) {
-        console.log('Aluno aprovado com a média: ' + media);
-    } else if (media < 7) {
-        console.log('Aluno reprovado com a média: ' + media);
-    }
-}
-
-function aluno(nome, curso) {
-    var mensagem = 'Seja bem vindo ' + nome + ' ao curso de ' + curso;
-
-    console.log(mensagem);
-}
-
-
